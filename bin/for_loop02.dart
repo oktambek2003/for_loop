e9224b79663521c3d7725a5f4cc5da00acf90acd
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-08 19:44:51 +0500

    2-misol

--- bin/for_loop02.dart
+++ bin/for_loop02.dart
@@ -1,4 +1,4 @@
-int func(List<int> numbers) {
+int func(List numbers) {
 /*
    Given a list of numbers, return the sum of the numbers at even index in the list
 Args:
@@ -6,7 +6,15 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  int i;
+  int b = 0;
+  for (int a = 0; a < numbers.length; a=a+2) {
+    i = numbers[a];
+    b = b + i;
+  }
+  return b;
 }
 
-void main() {}
+void main() {
+  print(func([1, 2, 2, 3, 4]));
+}
